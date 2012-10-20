2f3c57c13952f89388cf41d8adf7676d532ae708
Wojtek <wojtek.wk@gmail.com>
2012-10-20 17:03:25 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from neuralnetwork.Neuron import Neuron
+from neuralnetwork.Layer import Layer
+from neuralnetwork.Network import Network
+
+if __name__ == '__main__':
+    neuron = Neuron()
+    layer = Layer()
+    network = Network()
+    neuron.powiedzCosOSobie()
+    layer.powiedzCosOSobie()
+    network.powiedzCosOSobie()
