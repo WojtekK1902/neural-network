2eda6b4b2c281f0aedf8d989772e6642861956ca
Wojtek <wojtek.wk@gmail.com>
2012-11-12 22:26:53 +0100

    new version of configuration.data file

--- main.py
+++ main.py
@@ -30,80 +30,81 @@ if __name__ == '__main__':
             sys.exit(1)
         f = open(sys.argv[1],'r')
         line = f.readline().strip().split()
-        if len(line) < 2 or len(line) > 3:
+        if len(line) < 2:
             raise FileFormatException(f.tell())
         network = Network()
         network.inputs = int(line[0])
         network.outputs = int(line[len(line)-1])
-        network.hidden = len(line) - 2
-        print 'Liczba wejsc: ' + str(network.inputs)
-        print 'Liczba warstw ukrytych: ' + str(network.hidden)
-        print 'Liczba wyjsc: ' + str(network.outputs)
+        network.hidden = map(int, line[1:-1])
+        print 'Liczba wejsc:', str(network.inputs)
+        print 'Liczba warstw ukrytych:', str(len(network.hidden))
+        print 'Warstwy ukryte:', str(network.hidden)
+        print 'Liczba wyjsc:', str(network.outputs)
 
         #warstwa wejsciowa:
         l = f.readline().strip()
         if l:
             raise FileFormatException(f.tell())
-        layer = Layer()
-        layer.bias = (-1.0)*float(f.readline().strip())
-        network.layers.append(layer)
-        for i in range(network.inputs):
-            neuron = Neuron()
-            neuron.f = types.MethodType(liniowa, neuron)
-            l = f.readline().strip().split()
-            if not l:
-                raise FileFormatException(f.tell())
-            neuron.weights = map(float, l)
-            network.layers[-1].neurons.append(neuron)
-
-        #warstwy ukryte:
-        for i in range(network.hidden):
-            l = f.readline().strip()
-            if l:
-                raise FileFormatException(f.tell())
-            layer = Layer()
-            layer.bias = (-1.0)*float(f.readline().strip())
-            network.layers.append(layer)
-            line = f.readline().strip().split()
-            if not line:
-                raise FileFormatException(f.tell())
-            neurons_count = 0
-            while line:
-                neuron = Neuron()
-                l = line[:-1]
-                if neurons_count == 0:
-                    neurons_count = len(l)
-                if len(l) != neurons_count:
-                    raise FileFormatException(f.tell())
-                neuron.weights = map(float, l)
-                neuron.f = types.MethodType(FUNCTIONS[line[-1]], neuron)
-                network.layers[-1].neurons.append(neuron)
-                line = f.readline().strip().split()
-
-        if(network.hidden == 0): #ugly hack :/
-            f.readline()
-
-        #warstwa wyjsciowa:
-        layer = Layer()
-        network.layers.append(layer)
-        for i in range(network.outputs):
-            neuron = Neuron()
-            neuron.f = types.MethodType(FUNCTIONS[f.readline().strip()], neuron)
-            network.layers[-1].neurons.append(neuron)
-            
-        f.close()
-
-        input = []
-        print
-        for i in range(network.inputs):
-            input.append(float(raw_input('Input ' + str(i+1) + ': ')))
-
-        print
-        print "input: " + str(input)
-
-        output = network.compute(input)
-
-        print "odpowiedz sieci: " + str(output)
+##        layer = Layer()
+##        layer.bias = (-1.0)*float(f.readline().strip())
+##        network.layers.append(layer)
+##        for i in range(network.inputs):
+##            neuron = Neuron()
+##            neuron.f = types.MethodType(liniowa, neuron)
+##            l = f.readline().strip().split()
+##            if not l:
+##                raise FileFormatException(f.tell())
+##            neuron.weights = map(float, l)
+##            network.layers[-1].neurons.append(neuron)
+##
+##        #warstwy ukryte:
+##        for i in range(network.hidden):
+##            l = f.readline().strip()
+##            if l:
+##                raise FileFormatException(f.tell())
+##            layer = Layer()
+##            layer.bias = (-1.0)*float(f.readline().strip())
+##            network.layers.append(layer)
+##            line = f.readline().strip().split()
+##            if not line:
+##                raise FileFormatException(f.tell())
+##            neurons_count = 0
+##            while line:
+##                neuron = Neuron()
+##                l = line[:-1]
+##                if neurons_count == 0:
+##                    neurons_count = len(l)
+##                if len(l) != neurons_count:
+##                    raise FileFormatException(f.tell())
+##                neuron.weights = map(float, l)
+##                neuron.f = types.MethodType(FUNCTIONS[line[-1]], neuron)
+##                network.layers[-1].neurons.append(neuron)
+##                line = f.readline().strip().split()
+##
+##        if(network.hidden == 0): #ugly hack :/
+##            f.readline()
+##
+##        #warstwa wyjsciowa:
+##        layer = Layer()
+##        network.layers.append(layer)
+##        for i in range(network.outputs):
+##            neuron = Neuron()
+##            neuron.f = types.MethodType(FUNCTIONS[f.readline().strip()], neuron)
+##            network.layers[-1].neurons.append(neuron)
+##            
+##        f.close()
+##
+##        input = []
+##        print
+##        for i in range(network.inputs):
+##            input.append(float(raw_input('Input ' + str(i+1) + ': ')))
+##
+##        print
+##        print "input: " + str(input)
+##
+##        output = network.compute(input)
+##
+##        print "odpowiedz sieci: " + str(output)
         
     except FileFormatException as e:
         print 'Bad file format at position:', e.pos
