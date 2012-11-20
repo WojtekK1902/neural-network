c1c70a528e81f16a39b517bf4a702ee54cc5183d
Wojtek <wojtek.wk@gmail.com>
2012-11-20 23:59:00 +0100

    learning without neighbourhood and conscience

--- kohonen/Kohonen.py
+++ kohonen/Kohonen.py
@@ -2,6 +2,7 @@ import random
 
 class Kohonen(object):
     def __init__(self, width=3, height=3, outputs=4):
+        #TODO: dodac bias!
         #TODO: ewentualnie ustawianie alfa
         self.alfa = 0.06
         self.width = width
@@ -11,6 +12,35 @@ class Kohonen(object):
     def initialize(self):
         random.seed()
         self.weights = [[[random.random() for i in range(self.width)] for j in range(self.height)] for k in range(self.outputs)]
+        self.print_network()
+
+    def step(self, pict, k):
+        for j in range(self.height):
+            for i in range(self.width):
+                self.weights[k][j][i] += self.alfa*(pict[j][i]-self.weights[k][j][i])
+
+    def winner(self, pict):
+        max_result = float('-infinity')
+        max_result_ind = -1
+        for k, w in enumerate(self.weights):
+            result = 0.0
+            for j in range(self.height):
+                for i in range(self.width):
+                    result += w[j][i] * pict[j][i]
+            if result > max_result:
+                max_result = result
+                max_result_ind = k
+        return max_result_ind
+            
+
+    def learn(self, X, epochs=32000):
+        for e in range(epochs):
+            for pict in X:
+                k = self.winner(pict)
+                self.step(pict, k)
+        self.print_network()
+
+    def print_network(self):
         for k in range(self.outputs):
             print
             for j in range(self.height):
--- kohonen/main.py
+++ kohonen/main.py
@@ -3,3 +3,19 @@ from Kohonen import Kohonen
 k = Kohonen()
 
 k.initialize()
+f = open('training.data', 'r')
+#TODO: przeniesc czytanie wektora wejsciowego do Kohonen?
+X, w = [], []
+for line in f:
+    l = line.strip().split()
+    if len(l[0].split('&')) == 1:
+        if len(w) > 0:
+            X.append(w)
+        w = []
+    l[0] = l[0].strip('&')
+    w.append(map(float, l))
+X.append(w)
+
+print X
+f.close()
+k.learn(X)
