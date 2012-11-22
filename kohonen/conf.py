9b9bec7d6016aaf34e1c5b137a41de51f01e4009
Maciek Kazirod <maxfcb@gmail.com>
2012-11-22 23:10:44 +0100

    config moved to separate file

---
+++ kohonen/conf.py
@@ -0,0 +1,6 @@
+width = 3
+height = 3
+outputs = 4
+alfa = 0.06
+r = 7
+beta = 0.1
--- kohonen/main.py
+++ kohonen/main.py
@@ -1,14 +1,17 @@
+# coding=utf-8
 from Kohonen import Kohonen
 import sys
 
-if len(sys.argv) < 2:
-    print 'Brakuje pliku treningowego :('
+if len(sys.argv) < 3:
+    print 'Zbyt mało argumentów'
     sys.exit(1)
 
-k = Kohonen()
+exec("from kohonen import "+ sys.argv[2] + " as conf")
+
+k = Kohonen(conf)
 
 k.initialize()
-f = open('training.data', 'r')
+f = open(sys.argv[1], 'r')
 #TODO: przeniesc czytanie wektora wejsciowego do Kohonen?
 X, w = [], []
 for line in f:
